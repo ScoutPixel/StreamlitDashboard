034f8e047adb74634444b5a9c24b86804d4ac7b8
Eman A. M. Amer <eman.al.amer05@gmail.com>
2025-08-30 16:28:30 -0400

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,18 +1,3 @@
-# app.py
-import streamlit as st
-
-st.title("Hello Streamlit-er 👋")
-st.markdown(
-    """ 
-    This is a playground for you to try Streamlit and have fun. 
-
-    **There's :rainbow[so much] you can build!**
-    
-    We prepared a few examples for you to get started. Just 
-    click on the buttons above and discover what you can do 
-    with Streamlit. 
-    """
-)
-
-if st.button("Send balloons!"):
-    st.balloons()
+# My First App
+import streamlit as st # This line imports the a python library and give it a common nickname, st.
+st.title("Hello World!")
